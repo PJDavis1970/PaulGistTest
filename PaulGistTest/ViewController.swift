0e592b36e23715c5792c46dc982260e624301834
Paul Davis <pauld@dogfi.sh>
2018-10-08 12:16:50 +0100

    Initial Commit

---
+++ PaulGistTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaulGistTest
+//
+//  Created by Paul Davis on 08/10/2018.
+//  Copyright © 2018 pauld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
